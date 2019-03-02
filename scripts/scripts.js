4d48f212c8562742d887ac43f812b3d47086cf63
alisayuldybaeva <46886480+alisayuldybaeva@users.noreply.github.com>
2019-03-02 16:56:27 +0700

    owl
    owl

---
+++ scripts/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+externalLinks();
+});
+function externalLinks(){
+    $('a.new-window').bind('click', function(e){
+      var location = $(this).attr('href');  
+      window.open(location);
+      e.preventDefault();
+    });
+}
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    lazyLoad: true,
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoWidth:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  });
+});
+
